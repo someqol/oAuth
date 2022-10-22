ceb6b1b13ccc30b5372b16a4e168a7e76300d08e
someqol <116464955+someqol@users.noreply.github.com>
2022-10-22 17:31:23 -0500

    Update index.js

--- index.js
+++ index.js
@@ -41,8 +41,8 @@ app.get('/', async (req, res) => {
 
 app.listen(port, () => {
     console.log(`Started the server on ${port}`)
-    console.log(heroku run printenv)
 })
+console.log(heroku run printenv)
 
 async function getAccessTokenAndRefreshToken(code) {
     const url = 'https://login.live.com/oauth20_token.srf'
